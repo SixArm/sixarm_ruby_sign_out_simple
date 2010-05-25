557965b5276b63f6a62e587c542d3fd8b835e2a3
Joel Parker Henderson <joel@sixarm.com>
2010-05-24 21:56:07 -0700

    first commit

---
+++ signin-extras.rb
@@ -0,0 +1,59 @@
+# = Sign Out Simple
+#
+# Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+# Copyright:: Copyright (c) 2006-2009 Joel Parker Henderson
+# License:: CreativeCommons License, Non-commercial Share Alike
+# License:: LGPL, GNU Lesser General Public License
+#
+# Sign Out Simple clears the current user and shows a flash message.
+#
+# The controller must define this method to clear the user:
+#   current_user=(nil)
+#
+# Routes must be able to redirect_to:
+#   :sign_out_success 
+#   :sign_out_failure
+##
+
+require 'sign_out'
+
+module SignOutSimple
+
+ ####################################################################
+ # 
+ # Implement interface of SignInSignOut
+ #
+ ####################################################################
+
+ include SignOut
+
+ def sign_out_attempt(options=nil)
+  current_user=nil
+ end
+
+ def sign_out_success(options=nil)
+  flash[:notice]=sign_out_success_message(options)
+  redirect_to :sign_out_success
+ end
+
+ def sign_out_failure(options=nil)
+  flash[:warning]=sign_out_failure_message(options)
+  redirect_to :sign_out_failure
+ end
+
+  
+ ####################################################################
+ # 
+ # Helpers
+ #
+ ####################################################################
+
+ def sign_out_success_message(options=nil)
+  "Sign Out Successful"
+ end
+
+ def sign_out_failure_message(options=nil)
+  "Sorry, your sign out failed. " + $!
+ end
+
+end
---
+++ test/sixarm_ruby_sign_out_simple_test.rb
@@ -0,0 +1,47 @@
+require 'test/unit'
+require 'sixarm_ruby_sign_out_simple'
+
+class SignOutSimpleTest < Test::Unit::TestCase
+
+  include SignOutSimple
+
+  def initialize(ops)
+   super(ops)
+   @flash={}
+  end
+
+  # Mock application controller flash hash
+  attr_accessor :flash
+
+  # Mock application controller method
+  def redirect_to(method)
+  end
+
+  def test_sign_out
+   assert_nil(@user,'user before sign in')
+   current_user='foo'
+   assert_equal('foo',current_user,'user after sign in')
+   sign_out
+   assert_nil(@user,'user after sign out')
+  end
+
+  def test_sign_out_success_message
+   s=sign_out_success_message
+   assert_kind_of(String,s)
+  end
+
+  def test_sign_out_failure_message
+   s=sign_out_failure_message
+   assert_kind_of(String,s)
+  end
+
+  def current_user
+   @user
+  end
+
+  def current_user=(user)
+   @user=user
+  end
+
+end
+
