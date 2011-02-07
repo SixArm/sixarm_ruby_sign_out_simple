4e0f3a2324c9cdd04ba00b3ee4650abf5865b92c
SixArm <sixarm@sixarm.com>
2011-02-06 18:41:57 -0800

    updates for Ruby 1.9.2 Rakefile testing

--- lib/sixarm_ruby_sign_out_simple.rb
+++ lib/sixarm_ruby_sign_out_simple.rb
@@ -1,11 +1,11 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 =begin rdoc
 
-= SixArm Ruby Gem » SignOutSimple
+= SixArm.com » Ruby » SignOutSimple gem for typical Rails user sign out
 
 Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
-License:: Your choice of BSD, MIT, LGPL, or CreativeCommons Non-commercial Share Alike
+Copyright:: Copyright (c) 2006-2011 Joel Parker Henderson
+License:: See LICENSE.txt file
 
 SignOutSimple provides typical sign out for a Rails web application.
 
--- signin-extras.rb
+++ signin-extras.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#
 # = Sign Out Simple
 #
 # Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
